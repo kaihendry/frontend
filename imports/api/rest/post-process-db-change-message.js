c8d304366d5f08af0872194e5f899642bccf7d16
Kai Hendry <kai.hendry@unee-t.com>
2018-06-19 17:25:58 +0800

    Fix https://github.com/unee-t/sns2email/issues/1

--- imports/api/rest/post-process-db-change-message.js
+++ imports/api/rest/post-process-db-change-message.js
@@ -3,6 +3,7 @@ import { Email } from 'meteor/email'
 import MessagePayloads from '../message-payloads'
 import caseUserInvitedTemplate from '../../email-templates/user-invited-to-case'
 import caseAssigneeUpdateTemplate from '../../email-templates/case-assignee-updated'
+import caseNewTemplate from '../../email-templates/case-new'
 
 export default (req, res) => {
   if (req.query.accessToken !== process.env.API_ACCESS_TOKEN) {
@@ -21,18 +22,39 @@ export default (req, res) => {
     case_id: caseId
   } = message
 
-  var emailAddr, emailContent
+  var emailAddr, emailContent, assigneeId, assignee
 
   switch (type) {
+    case 'case_new':
+      // https://github.com/unee-t/sns2email/issues/1
+      // When a new case is created, we need to inform the person who is assigned to that case.
+
+      assigneeId = message.assignee_user_id
+      assignee = Meteor.users.findOne({'bugzillaCreds.id': parseInt(assigneeId)})
+      if (!assignee) {
+        console.error('Could deliver message to missing user of BZ ID: ' + assigneeId)
+        return
+      }
+      emailAddr = assignee.emails[0].address
+      emailContent = caseNewTemplate(assignee, caseTitle, caseId)
+      try {
+        Email.send(Object.assign({
+          to: emailAddr,
+          from: process.env.FROM_EMAIL
+        }, emailContent))
+        console.log('Sent', emailAddr, 'notification type:', type)
+      } catch (e) {
+        console.error(`An error ${e} occurred while sending an email to ${emailAddr}`)
+      }
+
+      break
+
     case 'case_assignee_updated':
       // https://github.com/unee-t/sns2email/issues/2
       // When the user assigned to a case change, we need to inform the person who is the new assignee to that case.
 
-      const {
-        assignee_user_id: assigneeId
-      } = message
-
-      const assignee = Meteor.users.findOne({'bugzillaCreds.id': parseInt(assigneeId)})
+      assigneeId = message.assignee_user_id
+      assignee = Meteor.users.findOne({'bugzillaCreds.id': parseInt(assigneeId)})
       if (!assignee) {
         console.error('Could deliver message to missing user of BZ ID: ' + assigneeId)
         return

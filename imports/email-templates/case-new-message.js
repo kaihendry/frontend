f66e4f42373df558aba48ae577d178d45ec470b8
Kai Hendry <kai.hendry@unee-t.com>
2018-07-03 17:02:57 +0800

    Fix https://github.com/unee-t/frontend/issues/342

--- imports/email-templates/case-new-message.js
+++ imports/email-templates/case-new-message.js
@@ -1,5 +1,18 @@
 import url from 'url'
 import { optOutHtml, optOutText } from './components/helpers'
+import { matchWidth } from '../util/cloudinary-transformations'
+
+function linkAttachment (message) {
+  const msgParts = message.split(' ')
+  if (msgParts.length > 1 && msgParts[0] === '[!attachment]') {
+    const attachmentUrl = msgParts[1]
+    if (attachmentUrl.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+      const thumbUrl = matchWidth(attachmentUrl, 500)
+      return `<a href=${thumbUrl}><img alt="Attachment image" src=${thumbUrl}></a>`
+    }
+  }
+  return message
+}
 
 export default (assignee, notificationId, settingType, caseTitle, caseId, userId, message) => ({
   subject: `New message on case "${caseTitle}"`,
@@ -9,7 +22,7 @@ export default (assignee, notificationId, settingType, caseTitle, caseId, userId
 
 <p>New message by ${userId.profile.name}:</p>
 
-<p>${message}</p>
+<p>${linkAttachment(message)}</p>
 
 <p>Please follow <a href='${url.resolve(process.env.ROOT_URL, `/case/${caseId}`)}'>${url.resolve(process.env.ROOT_URL, `/case/${caseId}`)}</a> to participate.</p>
 

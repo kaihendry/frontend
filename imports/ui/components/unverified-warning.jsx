edd5bd4872b61e7d39d23959c530bcb2fc0f52b9
Kai Hendry <kai.hendry@unee-t.com>
2018-07-25 15:00:09 +0800

    Data bind done

--- imports/ui/components/unverified-warning.jsx
+++ imports/ui/components/unverified-warning.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { createContainer } from 'meteor/react-meteor-data'
 
-export class UnverifiedWarning extends React.Component {
+class UnverifiedWarning extends React.Component {
   render () {
-    return (
+    const { isUserVerified, email } = this.props
+    return !isUserVerified ? (
       <main>
         <h1 className='f-1'>Verify your email address</h1>
-        <p>Your email address .. has not been verified yet.
-      Confirm your email to receive updates on your cases.</p>
+        <p>
+          Your email address {email} has not been verified yet.
+          Confirm your email to receive updates on your cases.
+        </p>
       </main>
-    )
+    ) : null
   }
 }
+
+UnverifiedWarning.propTypes = {
+  isUserVerified: PropTypes.bool.isRequired,
+  email: PropTypes.string.isRequired
+
+}
+
+export default connect(
+  () => ({}) // Redux store to props
+)(createContainer(
+  () => {
+    console.log('emails', Meteor.user() && Meteor.user().emails)
+    return {
+      isUserVerified: Meteor.user() ? Meteor.user().emails[0].verified : true,
+      email: Meteor.user() ? Meteor.user().emails[0].address : ''
+    }
+  }, // Meteor data to props
+  UnverifiedWarning
+))

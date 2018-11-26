36d9f739bfd4fea60916c47b96b5e5c4ee7e2d6e
Kai Hendry <kai.hendry@unee-t.com>
2018-11-26 09:06:27 +0800

    Set Cloudinary transformation with default

--- imports/util/cloudinary-transformations.js
+++ imports/util/cloudinary-transformations.js
@@ -1,4 +1,8 @@
-const generalReplacer = (url, paramsStr) => url.replace(/\/upload\/[^/]*\//, `/upload/f_auto/${paramsStr}/`)
+// https://cloudinary.com/documentation/image_transformations#automatic_format_selection
+const defaultTransform = 'f_auto'
+
+const generalReplacer = (url, paramsStr) => url.replace(/\/upload\/[^/]*\//,
+  `/upload/${defaultTransform + (paramsStr ? ',' + paramsStr : '')}/`)
 
 export const matchWidth = (url, pixelWidth) => generalReplacer(url, `w_${pixelWidth}`)
 export const fitDimensions = (url, pixelWidth, pixelHeight) =>

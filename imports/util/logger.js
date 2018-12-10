6e9df4a18a13c59819571687493e0da6bce11d38
Kai Hendry <kai.hendry@unee-t.com>
2018-12-10 16:23:36 +0800

    Tweaks to the logging

--- imports/util/logger.js
+++ imports/util/logger.js
@@ -1,6 +1,18 @@
 // https://github.com/baryon/tracer/issues/43
 export const logger = require('tracer')
   .console(
+    // https://github.com/baryon/tracer/issues/91#event-1968798299
     {
-      format: "{timestamp: '{{timestamp}}', title: '{{title}}', file: '{{file}}', line:'{{line}}', method: '{{method}}', message: '{{message}}' }"
+    // format: "{timestamp: '{{timestamp}}', title: '{{title}}', file: '{{file}}', line:'{{line}}', method: '{{method}}', message: '{{message}}' }"
+      transport: function (data) {
+        // if you dont need JSON.stringify, just remove it
+        console.log(JSON.stringify({
+          timestamp: data.timestamp,
+          title: data.title,
+          file: data.file,
+          line: data.line,
+          method: data.method,
+          message: data.message
+        }))
+      }
     })
